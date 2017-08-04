f90636e0a8ae317da96ecc387954835945de68b1
xiUltron <ischenxi@gmial.com>
2017-08-04 17:11:24 +0800

    update static_pages.blade.php

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,10 +1,9 @@
-<?php
-/**
-* Created by PhpStorm.
-* User: ultron
-* Date: 2017/8/3
-* Time: 17:31
-*/
+{{--/**--}}
+{{--* Created by PhpStorm.--}}
+{{--* User: ultron--}}
+{{--* Date: 2017/8/3--}}
+{{--* Time: 17:31--}}
+{{--*/--}}
 @extends('layouts.default')
 @section('content')
     <h1>帮助页</h1>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,10 +1,9 @@
-
-/**
-* Created by PhpStorm.
-* User: ultron
-* Date: 2017/8/3
-* Time: 17:31
-*/
+{{--/**--}}
+{{--* Created by PhpStorm.--}}
+{{--* User: ultron--}}
+{{--* Date: 2017/8/3--}}
+{{--* Time: 17:31--}}
+{{--*/--}}
 @extends('layouts.default')
 @section('content')
     <h1>主页</h1>
