327b67f9e051e10441474a5a8a0710ec83307221
xiUltron <ischenxi@gmial.com>
2017-08-05 11:28:21 +0800

    update css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,10 +8,25 @@
 ?>
         <!doctype html>
 <html>
-    <head>
-        <title>@yield('title', 'Sample') —— Laravel Study</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
+<head>
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title', 'Sample') —— Laravel Study</title>
+</head>
+<body>
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="/" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/help">帮助</a></li>
+                    <li><a href="#">登录</a></li>
+            </nav>
+        </div>
+    </div>
+</header>
+<div class="container">
+    @yield('content')
+</div>
+</body>
 </html>
