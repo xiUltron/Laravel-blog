ba924754e9edc15f68cb3066c55a10538bfe8088
xiUltron <ischenxi@gmial.com>
2017-08-07 16:22:34 +0800

    add footer blade

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ultron
+ * Date: 2017/8/7
+ * Time: 16:14
+ */
+?>
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34"
+                 class="brand-icon">
+            <a href="#">??</a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/about">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
