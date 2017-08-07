6c990268a41d62e587c00c4b4b5a89eabd0120b2
xiUltron <ischenxi@gmial.com>
2017-08-07 16:26:13 +0800

    update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,23 +10,13 @@
 <html>
 <head>
     <link rel="stylesheet" href="/css/app.css">
-    <title>@yield('title', 'Sample') —— Laravel Study</title>
+    <title>@yield('title', 'Sample') —— IT Skynet</title>
 </head>
 <body>
-<header class="navbar navbar-fixed-top navbar-inverse">
-    <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            <a href="/" id="logo">Sample App</a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/help">帮助</a></li>
-                    <li><a href="#">登录</a></li>
-            </nav>
-        </div>
-    </div>
-</header>
+@include('layouts._header')
 <div class="container">
     @yield('content')
+    @include('layouts._footer')
 </div>
 </body>
 </html>
