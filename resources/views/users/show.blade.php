9586dbb6305e23f03105a341b192fa86b4ea6690
xiUltron <ischenxi@gmial.com>
2017-08-09 17:37:42 +0800

    update show.blade.php

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ultron
+ * Date: 2017/8/9
+ * Time: 17:35
+ */
+?>
+
+@extends('layouts.default')
+@section('title',$user->name)
+@section('content')
+    {{ $user->name }} - {{ $user->email }}
+@stop
