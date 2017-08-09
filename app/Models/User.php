9b42bd8c6497e0e00e5f3ab1577db61aedb63341
xiUltron <ischenxi@gmial.com>
2017-08-09 18:00:40 +0800

    update user show

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
      * @var string
      */
     protected $table = 'users';
+
+    public function gravatar($size = 100) {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
---
+++ resources/views/shared/_user_info.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ultron
+ * Date: 2017/8/9
+ * Time: 17:55
+ */
+?>
+
+<a href="{{ route('user.show', $user->id) }}">
+    <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar" />
+</a>
+<h1>{{ $user->name }}</h1>
