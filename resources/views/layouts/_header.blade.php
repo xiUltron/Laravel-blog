94713976105ad818cf7b2a37f304a2353b0c31ab
xiUltron <ischenxi@gmial.com>
2017-08-31 17:30:25 +0800

    modify _header.blade.php

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -13,7 +13,7 @@
             <nav>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="{{ route('help') }}">帮助</a></li>
-                    <li><a href="#">登录</a></li>
+                    <li><a href="{{ route('login') }}">登录</a></li>
                 </ul>
             </nav>
         </div>
