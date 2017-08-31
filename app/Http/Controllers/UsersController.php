8b3aa3839828d8c751def5d9c612a2730eb935e3
xiUltron <ischenxi@gmial.com>
2017-08-31 10:43:14 +0800

    update language

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,7 +25,15 @@ class UsersController extends Controller
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required'
         ]);
-        return;
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->rmail,
+            'password'=>bcrypt($request->password),
+        ]);
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程');
+        return redirect()->route('users.show',[$user]);
     }
 
 }
---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ultron
+ * Date: 2017/8/31
+ * Time: 10:35
+ */
+?>
+
+@foreach(['danger', 'warning', 'success', 'info'] as $msg)
+    @if(session()->has($msg))
+        <div class="flash-message">
+            <p class="alert alert-{{ $msg }}">
+                {{ session()->get($msg) }}
+            </p>
+        </div>
+    @endif
+@endforeach
