21e65f9a8696bbe4bdd5af3dee2ffb77cd704df7
xiUltron <ischenxi@gmial.com>
2017-08-31 17:28:18 +0800

    add login page

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ultron
+ * Date: 2017/8/31
+ * Time: 14:33
+ */
+?>
+
+@section('content')
+    <div class="col-md-offset-2 col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h5>登录</h5>
+            </div>
+            <div class="panel-body">
+                @include('shared._errors')
+
+                <form action="{{ route('login') }}" method="post">
+                    <div class="form-group">
+                        <label for="email">邮箱：</label>
+                        <input type="text" class="form-control" name="email" value="{{ old('email') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">密码：</label>
+                        <input type="password" class="form-control" name="password" value="{{ old('password') }}">
+                    </div>
+
+                    <button class="btn btn-primary" type="submit">登录</button>
+                    <button class="btn btn-pri" id="thisisID" type="submit" name="btnName">登录</button>
+                </form>
+
+                <hr>
+
+                <p>还没有账号？<a href="{{ route('signup') }}">现在注册！</a></p>
+            </div>
+        </div>
+    </div>
+@stop
