3748566d710f1a7a014182a35e458775b65261fa
xiUltron <ischenxi@gmial.com>
2017-09-04 11:37:54 +0800

    fix bug

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -15,9 +15,11 @@
 <body>
 @include('layouts._header')
 <div class="container">
-    @include('shared._messages')
-    @yield('content')
-    @include('layouts._footer')
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
 <script src="/js/app.js/"></script>
 </body>
