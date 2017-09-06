5ba70487bc7768920e4247df88f1481eae3f5121
xiUltron <ischenxi@gmial.com>
2017-09-06 16:23:12 +0800

    fix bugs

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -6,68 +6,31 @@
  * Time: 14:27
  */
 ?>
-{{--<header class="navbar navbar-fixed-top navbar-inverse">--}}
-    {{--<div class="container">--}}
-        {{--<div class="col-md-offset-1 col-md-10">--}}
-            {{--<a href="/" id="logo">IT Skynet</a>--}}
-            {{--<nav>--}}
-                {{--<ul class="nav navbar-nav navbar-right">--}}
-                    {{--@if(\Illuminate\Support\Facades\Auth::check())--}}
-                        {{--<li><a href="#">用户列表</a></li>--}}
-                        {{--<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
-                                {{--{{ \Illuminate\Support\Facades\Auth::user()->name }}<b class="caret"></b>--}}
-                            {{--</a>--}}
-                            {{--<ul class="dropdown-menu">--}}
-                                {{--<li>--}}
-                                    {{--<a href="{{ route('users.show', \Illuminate\Support\Facades\Auth::user()->id) }}">个人中心</a>--}}
-                                {{--</li>--}}
-                                {{--<li><a href="{{ route('users.edit', \Illuminate\Support\Facades\Auth::user()->id) }}">编辑资料</a>--}}
-                                {{--</li>--}}
-                                {{--<li class="divider"></li>--}}
-                                {{--<li>--}}
-                                    {{--<a href="#" id="logout">--}}
-                                        {{--<form action="{{ route('logout') }}" method="post">--}}
-                                            {{--{{ csrf_field() }}--}}
-                                            {{--{{ method_field('DELETE') }}--}}
-                                            {{--<button class="btn btn-block btn-danger" type="submit" name="button">退出--}}
-                                            {{--</button>--}}
-                                        {{--</form>--}}
-                                    {{--</a>--}}
-                                {{--</li>--}}
-                            {{--</ul>--}}
-                        {{--</li>--}}
-                    {{--@else--}}
-                        {{--<li><a href="{{ route('help') }}">帮助</a></li>--}}
-                        {{--<li><a href="{{ route('login') }}">登录</a></li>--}}
-                    {{--@endif--}}
-                {{--</ul>--}}
-            {{--</nav>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</header>--}}
-
 <header class="navbar navbar-fixed-top navbar-inverse">
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
-            <a href="/" id="logo">Sample App</a>
+            <a href="/" id="logo">IT Skynet</a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::check())
+                    @if(\Illuminate\Support\Facades\Auth::check())
                         <li><a href="#">用户列表</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                {{ Auth::user()->name }} <b class="caret"></b>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                {{ \Illuminate\Support\Facades\Auth::user()->name }}<b class="caret"></b>
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a href="{{ route('users.show', Auth::user()->id) }}">个人中心</a></li>
-                                <li><a href="{{ route('users.edit', Auth::user()->id) }}">编辑资料</a></li>
+                                <li>
+                                    <a href="{{ route('users.show', \Illuminate\Support\Facades\Auth::user()->id) }}">个人中心</a>
+                                </li>
+                                <li><a href="{{ route('users.edit', \Illuminate\Support\Facades\Auth::user()->id) }}">编辑资料</a>
+                                </li>
                                 <li class="divider"></li>
                                 <li>
-                                    <a id="logout" href="#">
-                                        <form action="{{ route('logout') }}" method="POST">
+                                    <a href="#" id="logout">
+                                        <form action="{{ route('logout') }}" method="post">
                                             {{ csrf_field() }}
                                             {{ method_field('DELETE') }}
-                                            <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                            <button class="btn btn-block btn-danger" type="submit" name="button">退出
+                                            </button>
                                         </form>
                                     </a>
                                 </li>
