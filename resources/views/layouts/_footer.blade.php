de1b4b47fc1830f74cacd0db4a74a13c7eb54db6
xiUltron <ischenxi@gmial.com>
2017-09-06 12:04:08 +0800

    update _footer style

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -8,7 +8,7 @@
 ?>
 <div class="col-md-12">
     <footer class="footer">
-        <small class="slogon">
+        <small class="glyphicon-copyright-mark">
             <img src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34"
                  class="brand-icon">
             <a href="#">IT Skynet</a>
