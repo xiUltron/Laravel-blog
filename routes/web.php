5cfb061f37fc345fcf65142a686fdfe4876c7c7b
xiUltron <ischenxi@gmial.com>
2017-09-06 17:01:09 +0800

    general update

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-//Route::get('signup', 'StaticPagesController@signup')->name('signup');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 
