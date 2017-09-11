9ac2c10097eefb1d28bbcac689d4eba7156b68ea
xiUltron <ischenxi@gmial.com>
2017-09-11 17:43:34 +0800

    update

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -9,12 +9,13 @@
 @section('content')
     <div class="jumbotron">
         <h1>IT Skynet</h1>
-        <p class="lead">
-            欢迎，
-        </p>
         @if(Auth::check())
             <p class="lead">
-                {{ Auth::user()->name }}
+                欢迎 {{ Auth::user()->name }}
+            </p>
+        @else
+            <p class="lead">
+                亲爱的游客
             </p>
         @endif
         <p class="lead">
