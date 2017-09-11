2176185c90a90218f676a4ad9399fa483fc1e851
xiUltron <ischenxi@gmial.com>
2017-09-11 17:17:54 +0800

    update authorize

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -34,7 +34,7 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         } else {
             session()->flash('danger', '很抱歉！邮箱或密码有误！');
             return redirect()->back();
