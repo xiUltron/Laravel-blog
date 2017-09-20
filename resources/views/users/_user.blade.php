105bdc0749abb641a2b5e4a7b80d5718b39eb4d7
xiUltron <ischenxi@gmial.com>
2017-09-20 17:38:19 +0800

    update

---
+++ resources/views/users/_user.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ultron
+ * Date: 2017/9/20
+ * Time: 17:34
+ */
+?>
+
+<li>
+    <img src="{{ $user->gravatar() }}" alt="{{ $user-name }}" class="gravatar">
+    <a href="{{ route('users.show', $user->id) }}" class="username">{{ $user->name }}</a>
+</li>
