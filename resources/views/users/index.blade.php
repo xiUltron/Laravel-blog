ba016c10e6486a03ef0b9069f6fe879b35a75b52
xiUltron <ischenxi@gmial.com>
2017-09-20 17:22:00 +0800

    update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -18,6 +18,7 @@
                 <li>
                     <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar">
                     <a href="{{ route('users.show', $user->id) }}" class="username">{{ $user->name }}</a>
+                    {{ $user->created_at }}
                 </li>
             @endforeach
         </ul>
