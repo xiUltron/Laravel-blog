a3de050b0645fc397188ec38b077cbcd44f3d907
xiUltron <ischenxi@gmial.com>
2017-09-20 16:49:49 +0800

    update

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,12 +13,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
     static $password;
 
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => $faker->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
