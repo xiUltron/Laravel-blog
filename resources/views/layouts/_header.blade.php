04457edf15a2d332d9da24083035965400301e17
xiUltron <ischenxi@gmial.com>
2017-09-21 16:18:30 +0800

    update css

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -12,7 +12,7 @@
         var win=$(window); //得到窗口对象
         var sc=$(document);//得到document文档对象。
         win.scroll(function(){
-            if(sc.scrollTop()>=nav.offsetTop){
+            if(sc.scrollTop()>=nav.offsetTop()){
                 nav.addClass("fixednav");
                 $(".navTmp").fadeIn();
             }else{
