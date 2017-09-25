e31fc73229e1e90664febbf0253cae452174010b
xiUltron <ischenxi@gmial.com>
2017-09-25 17:01:14 +0800

    update scss

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -8,25 +8,27 @@
 @section('title','主页')
 {{--@section('background', 'background=http://orxfb7mt0.bkt.clouddn.com/slider2.jpg')--}}
 @section('content')
-    <div class="jumbotron" style="background: url(http://orxfb7mt0.bkt.clouddn.com/slider2.jpg)">
-        <h1>IT Skynet</h1>
-        @if(Auth::check())
+    <div style="background: url(http://orxfb7mt0.bkt.clouddn.com/slider2.jpg)">
+        <div class="jumbotron">
+            <h1>IT Skynet</h1>
+            @if(Auth::check())
+                <p class="lead">
+                    欢迎 {{ Auth::user()->name }}
+                </p>
+            @else
+                <p class="lead">
+                    亲爱的游客
+                </p>
+            @endif
             <p class="lead">
-                欢迎 {{ Auth::user()->name }}
+                你现在看到的是<a href="http://www.itskynet.com">Laravel 入门教程</a> 的示例项目主页。
             </p>
-        @else
-            <p class="lead">
-                亲爱的游客
+            <p>
+                一切，将从这里开始。
             </p>
-        @endif
-        <p class="lead">
-            你现在看到的是<a href="http://www.itskynet.com">Laravel 入门教程</a> 的示例项目主页。
-        </p>
-        <p>
-            一切，将从这里开始。
-        </p>
-        <p>
-            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
-        </p>
+            <p>
+                <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+            </p>
+        </div>
     </div>
 @stop
