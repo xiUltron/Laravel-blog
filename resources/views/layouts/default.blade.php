11fb940ea4c04ae2e9e7897a1258a2e7c2f0f28c
xiUltron <ischenxi@gmial.com>
2017-09-26 12:08:27 +0800

    update scss

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -18,8 +18,8 @@
     <div class="col-md-offset-1 col-md-10">
         @include('shared._messages')
         @yield('content')
-        @include('layouts._footer')
     </div>
+    @include('layouts._footer')
 </div>
 <script src="/js/app.js"></script>
 </body>
