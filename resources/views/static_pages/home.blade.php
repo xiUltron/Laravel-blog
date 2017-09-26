3ef7030ab0a226189ea638b329511f1d5ff8acfa
xiUltron <ischenxi@gmial.com>
2017-09-26 11:54:01 +0800

    update scss

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,10 +6,9 @@
 {{--*/--}}
 @extends('layouts.default')
 @section('title','主页')
-{{--@section('background')--}}
-    {{--class="home-body"--}}
-{{--@stop--}}
-@section('background','background=http://orxfb7mt0.bkt.clouddn.com/140e89ff8a38d078ab4e3ee77c691f5d.jpg no-repeat')
+@section('background')
+    class="home-body"
+@stop
 @section('content')
     <div>
         <div class="jumbotron">
