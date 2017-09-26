d5cdafb78631fea7f2ba304e1943da4eb6f9a00e
xiUltron <ischenxi@gmial.com>
2017-09-26 11:52:02 +0800

    update scss test

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -9,7 +9,7 @@
 {{--@section('background')--}}
     {{--class="home-body"--}}
 {{--@stop--}}
-@section('background','background=http://orxfb7mt0.bkt.clouddn.com/140e89ff8a38d078ab4e3ee77c691f5d.jpg')
+@section('background','background=http://orxfb7mt0.bkt.clouddn.com/140e89ff8a38d078ab4e3ee77c691f5d.jpg no-repeat')
 @section('content')
     <div>
         <div class="jumbotron">
