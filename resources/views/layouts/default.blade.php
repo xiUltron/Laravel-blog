4084f966e92697ca11937e1004c408af8822fc02
xiUltron <ischenxi@gmial.com>
2017-09-26 11:13:13 +0800

    update scss

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" href="/css/app.css">
     <title>@yield('title', 'Sample') —— IT Skynet</title>
 </head>
-<body @yield('background', '') style="background: url("")">
+{{--<body @yield('background', '')>--}}
+<body class="home-body">
 @include('layouts._header')
 <div class="container">
     <div class="col-md-offset-1 col-md-10">
