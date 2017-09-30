fa6e35899958471535935c690bb338d822813ac1
xiUltron <ischenxi@gmial.com>
2017-09-30 16:19:57 +0800

    update

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -44,20 +44,27 @@ class SessionsController extends Controller
         ];
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来！');
-            return redirect()->intended(route('users.show', [Auth::user()]));
-//            return back();
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            } else {
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
         } else {
             session()->flash('danger', '很抱歉！邮箱或密码有误！');
             return redirect()->back();
         }
+
         return;
     }
 
     /**
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function destroy()
+    public
+    function destroy()
     {
         Auth::logout();
         session()->flash('success', '您已成功退出！');
---
+++ resources/views/emails/confirm.blade.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ultron
+ * Date: 2017/9/29
+ * Time: 10:12
+ */
+?>
+
+        <!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>注册确认链接</title>
+</head>
+<body>
+<h1>感谢在 itskynet 进行注册！</h1>
+<p>
+    请点击下面的链接完成注册
+    <a href="{{ route('confirm_email', '$user->activation_token') }}">
+        {{ route('confirm_email', '$user->activation_token') }}
+    </a>
+</p>
+<p>
+    如果这不是您本人的操作，请忽略此邮件。
+</p>
+</body>
+</html>
