9ea6af59602e9c94776760f9d0a094ab92d30dee
xiUltron <ischenxi@gmial.com>
2017-10-10 15:13:14 +0800

    update email

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -14,22 +14,22 @@
     <title>注册确认链接</title>
 </head>
 <body>
-<h1>{{ $user->name }} 您好！感谢在 itskynet 进行注册！</h1>
+<h2>{{ $user->name }} 您好！</h2>
 <p>
     我们即将完成您的账户创建。您可以使用此账户登录 www.itskynet.com。
 </p>
 <p>
-    登录邮箱：{{ $user->email }}
-    显示昵称：{{ $user->name }}
+    登录邮箱：{{ $user->email }}<br/>
+    显示名称：{{ $user->name }}
 </p>
 <p>
     请点击下面的链接完成注册
-    <a href="{{ route('confirm_email', $user->activation_token) }}">
-        {{ route('confirm_email', $user->activation_token) }}
+    <a href="{{ route('confirm_email', $user->activation_token) }}" class="btn btn-lg btn-success">
+        验证您的电子邮件
     </a>
 </p>
 <p>
-    如果这不是您本人的操作，请忽略此邮件。
+    如果你没有注册过，可放心忽略这封电子邮件。别人可能错误地键入了你的电子邮件地址。
 </p>
 </body>
 </html>
