92c0e43e8f748835a716f119356c9308f313803c
xiUltron <ischenxi@gmial.com>
2017-10-11 17:34:06 +0800

    update password reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -13,7 +13,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">重置密码</div>
                     <div class="panel-body">
@@ -42,7 +42,7 @@
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                                <div class="col-md-6">
                                     <button type="submit" class="btn btn-primary">发送密码重置邮件</button>
                                 </div>
                             </div>
