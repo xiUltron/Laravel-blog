63d1c5218aa978e1a6836bd83778cbf422e6b16c
xiUltron <ischenxi@gmial.com>
2017-10-16 15:21:52 +0800

    update --statuses

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,8 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         Schema::defaultStringLength(191);
+        Carbon::setLocale('zh');
     }
 
     /**
