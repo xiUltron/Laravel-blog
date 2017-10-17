c0a75fbae3baf449df851fcbe1ae17989c70c1f5
xiUltron <ischenxi@gmial.com>
2017-10-17 16:23:32 +0800

    update --public statuses

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -10,7 +10,7 @@
 <form action="{{ route('statuses.store') }}" method="post">
     @include('shared._errors')
     {{ csrf_field() }}
-    <textarea name="content" rows="3" class="form-control" placeholder="聊聊新鲜事儿......">
+    <textarea class="form-control" rows="3" placeholder="聊聊新鲜事儿..." name="content">
         {{ old('content') }}
     </textarea>
     <button type="submit" class="btn btn-primary pull-right">发布</button>
