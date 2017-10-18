b43cf09fad41f2e7f3342d113a3a8419554f144f
xiUltron <ischenxi@gmial.com>
2017-10-18 17:56:09 +0800

    update --publish jQuery

--- public/js/addStatus.js
+++ public/js/addStatus.js
@@ -1,5 +1,5 @@
 $('button[name="btnPublish"]').click( function () {
-    $.post('statuses/store', $('#statusPublishForm').val(), function (html) {
+    $.post('statuses', $('#statusPublishForm').val(), function (html) {
         $('.statuses').prepend(html)
     })
 });
--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -7,10 +7,18 @@
  */
 ?>
 
-<form action="{{ route('statuses.store') }}" method="post">
+{{--<form action="{{ route('statuses.store') }}" method="post">--}}
+    {{--@include('shared._errors')--}}
+    {{--{{ csrf_field() }}--}}
+    {{--<textarea class="form-control" rows="3" placeholder="聊聊新鲜事儿..."--}}
+              {{--name="content">{{ old('content') }}</textarea>--}}
+    {{--<button type="submit" class="btn btn-primary pull-right">发布</button>--}}
+{{--</form>--}}
+
+<div id="StatusPublishForm">
     @include('shared._errors')
     {{ csrf_field() }}
     <textarea class="form-control" rows="3" placeholder="聊聊新鲜事儿..."
               name="content">{{ old('content') }}</textarea>
-    <button type="submit" class="btn btn-primary pull-right">发布</button>
-</form>
+    <button type="button" class="btn btn-primary pull-right" name="btnPublish">发布</button>
+</div>
