439d9a007e52592bc9befed3bdd507c847ad3184
xiUltron <ischenxi@gmial.com>
2017-10-18 17:54:01 +0800

    update --publish jQuery

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -7,10 +7,10 @@
  */
 ?>
 
-<div id="StatusPublishForm">
+<form action="{{ route('statuses.store') }}" method="post">
     @include('shared._errors')
     {{ csrf_field() }}
     <textarea class="form-control" rows="3" placeholder="聊聊新鲜事儿..."
               name="content">{{ old('content') }}</textarea>
-    <button type="button" class="btn btn-primary pull-right" name="btnPublish">发布</button>
-</div>
+    <button type="submit" class="btn btn-primary pull-right">发布</button>
+</form>
