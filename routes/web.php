eaf5b471a81d241663a92b4c23fef3bda9c94f73
xiUltron <ischenxi@gmial.com>
2017-10-19 11:33:54 +0800

    update --publish jQuery

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,15 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+/* 上面的代码相当于：
+ *Route::get('/users', 'UsersController@index')->name('users.index');
+ *Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+ *Route::get('/users/create', 'UsersController@create')->name('users.create');
+ *Route::post('/users', 'UsersController@store')->name('users.store');
+ *Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+ *Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+ *Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+ */
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
@@ -32,6 +41,6 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
-//Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+//Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]); 相当于：
 Route::post('statuses/store', 'StatusesController@store')->name('statuses.store');
 Route::delete('statuses/{id}', 'StatusesController@destroy')->name('statuses.destroy');
