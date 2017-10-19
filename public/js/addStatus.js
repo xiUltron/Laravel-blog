30e17042c43e2ed9f29a172a0f462670d3ef032d
xiUltron <ischenxi@gmial.com>
2017-10-19 11:43:35 +0800

    update --publish jQuery

--- public/js/addStatus.js
+++ public/js/addStatus.js
@@ -4,9 +4,10 @@ $.ajaxSetup({
     }
 });
 $('button[name="btnPublish"]').click(function () {
-    $.post('statuses/store', $('#statusTextArea').val()
-    //     function (html) {
-    //     $('.statuses').prepend(html)
-    // }
+    $.post('statuses/store',
+        $('#statusTextArea').val(),
+        function (html) {
+            $('.statuses').prepend(html)
+        }
     )
 });
